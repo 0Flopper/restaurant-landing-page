2b510deb1ffa5da66358ee0b4d8fb456d85d89dd
Caique <caiquematos@proton.me>
2025-07-10 13:08:19 -0300

    Setup Home Page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Logo from '../assets/logo.png'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+        <Navbar />
+    </div>
+    
+  )
+}
+
+export default Home
