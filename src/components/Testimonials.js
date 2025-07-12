5812a89ebd574dc14ceba9ea884d016554117103
Caique <caiquematos@proton.me>
2025-07-12 09:43:35 -0300

    Add Testimonial Section

---
+++ src/components/Testimonials.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ProfilePic from '../Assets/john-doe-image.png'
+import { AiFillStar } from 'react-icons/ai'
+
+const Testmonials = () => {
+  return (
+    <div className='work-section-wrapper'>
+        <div className='work-section-top'>
+            <p className='primary-subheading'>Testmonials</p>
+            <h1 className='primary-heading'>What They Are Saying</h1>
+            <p className="primary-text">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean 
+                commodo ligula eget dolor. Aenean massa.
+            </p>
+        </div>
+        <div className='testimonial-section-bottom'>
+            <img src={ProfilePic} alt="Profile"/>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean 
+                commodo ligula eget dolor. Aenean massa.
+            </p>
+            <div className='testimonials-stars-container'>
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+            </div>
+            <h2>John Doe</h2> 
+        </div>
+    </div>
+  )
+}
+
+export default Testmonials
